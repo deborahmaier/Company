74c7bc69a0383bae94e75f8ee23d43f3b8eed6e7
Maier Sananez Deborah <debomaier@mi.unc.edu.ar>
2022-10-25 01:35:53 -0300

    .

--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -23,20 +23,9 @@ namespace WebApplication1
         
         public static void Main(string[] args)
         {
+            
             CreateHostBuilder(args).Build().Run();
-            
-            //PersonnelController pC = new();
-
-            //var csvPath = Path.Combine(Environment.CurrentDirectory, $"personnel -{DateTime.Now.ToFileTime()}.csv");
-            //using (var streamWriter = new StreamWriter(csvPath))
-            //{
-            //    using CsvWriter csvWriter = new(streamWriter, CultureInfo.InvariantCulture);
-            //    var personnel = pC.Get();
-            //    csvWriter.WriteRecords(personnel);
-            //}
-            
-
-           
+       
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
