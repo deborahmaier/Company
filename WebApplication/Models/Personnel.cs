291eb38cf575ff7df1932abf36f61702e00d9cd0
Maier Sananez Deborah <debomaier@mi.unc.edu.ar>
2022-10-25 00:12:31 -0300

    CRUD Personnel

--- WebApplication/Models/Personnel.cs
+++ WebApplication/Models/Personnel.cs
@@ -17,8 +17,10 @@ namespace WebApplication.Models
         public DateTime JoinedDate { get; set; }
         public int WorkingHours { get; set; }
         public int GrossIncome { get; set; }
+        public string Department { get; set; }
 
-        public Personnel (int personnelId, string name, string surname, DateTime birthdate, string birthPlace, string adress, string zipCode, DateTime joinedDate, int workingHours, int grossIncome)
+
+        public Personnel(int personnelId, string name, string surname, DateTime birthdate, string birthPlace, string adress, string zipCode, DateTime joinedDate, int workingHours, int grossIncome, string department)
         {
             this.PersonnelId = personnelId;
             this.Name = name;
@@ -30,10 +32,7 @@ namespace WebApplication.Models
             this.JoinedDate = joinedDate;
             this.WorkingHours = workingHours;
             this.GrossIncome = grossIncome;
-        }
-        public Personnel()
-        {
-
+            this.Department = department;
         }
     }
 }
